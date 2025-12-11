7a09cf8d1161d251be45818a8dc6d2f520394604
Karabo <karabooliphant34@gmail.com>
2025-12-11 20:15:53 +0200

    🔥 SIMPLIFY: Remove dark mode, use CDN only

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+// REMOVED: import "./index.css"; - We're using CDN now!
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
