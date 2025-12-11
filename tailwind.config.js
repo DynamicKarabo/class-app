59099d2786dabe663c9ddba3ec3f47c06e7a993e
Karabo <karabooliphant34@gmail.com>
2025-12-11 20:07:34 +0200

    fixed dark theme toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#4f46e5',
+        secondary: '#7c3aed',
+        accent: '#f472b6'
+      }
+    },
   },
   plugins: [],
 }
